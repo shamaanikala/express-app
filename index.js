439d3e2b308b39254880057b62dc0ecf1f39eda4
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-06-14 19:51:54 +0300

    Ex3.1: editing browser and console output

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hei maailma!</h1>')
+  res.send('<h1>Hei maailma! 😸</h1>')
 })
 
 const PORT = 8080
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+  console.log(`express-app: deployed with GitHub Actions & Watchtower`)
 })
